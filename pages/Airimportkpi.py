a58efd14eda36d4a1d29c7edd62077900f86055d
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-07-31 13:55:34 +0300

    Update Airimportkpi.py

--- pages/Airimportkpi.py
+++ pages/Airimportkpi.py
@@ -8,11 +8,13 @@ from yaml.loader import SafeLoader
 import openpyxl
 import os
 
+st.set_page_config(page_title='Genel Transport',page_icon="https://www.geneltransport.com.tr/wp-content/uploads/2021/03/favicon.png", layout='wide')
+st.image('https://www.geneltransport.com.tr/wp-content/uploads/2021/03/logo-color.png')
+
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-st.set_page_config(page_title='Genel Transport',page_icon="https://www.geneltransport.com.tr/wp-content/uploads/2021/03/favicon.png", layout='wide')
-st.image('https://www.geneltransport.com.tr/wp-content/uploads/2021/03/logo-color.png')
+
 
 
     
