186d40286eecde897424ff1cd2f2ec3c584357dc
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-07-31 13:54:08 +0300

    Update Air.py

--- pages/Air.py
+++ pages/Air.py
@@ -8,11 +8,14 @@ from yaml.loader import SafeLoader
 import openpyxl
 import numpy as np
 from streamlit_lightweight_charts import renderLightweightCharts
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
 
 st.set_page_config(page_title='Genel Transport',page_icon="https://www.geneltransport.com.tr/wp-content/uploads/2021/03/favicon.png", layout='wide')
 st.image('https://www.geneltransport.com.tr/wp-content/uploads/2021/03/logo-color.png')
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
 
 
     
