4f2eef87f7013c6c6f05090ec3d8bd62773bd0d7
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-07-31 13:46:19 +0300

    Update Airexporttarget.py

--- pages/Airexporttarget.py
+++ pages/Airexporttarget.py
@@ -14,16 +14,7 @@ with open('config.yaml') as file:
 st.set_page_config(page_title='Genel Transport',page_icon="https://www.geneltransport.com.tr/wp-content/uploads/2021/03/favicon.png", layout='wide')
 st.image('https://www.geneltransport.com.tr/wp-content/uploads/2021/03/logo-color.png')
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['pre-authorized']
-)
 
-authenticator.login()
-if st.session_state["authentication_status"]:
 
     
 
@@ -41,9 +32,7 @@ if st.session_state["authentication_status"]:
         st.page_link("pages/Clientanalitics.py",label="Client Offer/Success Analysis", icon="📈")
         st.page_link("pages/Clientaircustomer.py",label="Client Air Customer Offer Analysis", icon="📈")
 
-    st.sidebar.write(f'Welcome *{st.session_state["name"]}*')
-    authenticator.logout("Logout", "sidebar")
-
+  
 
     filenames = os.listdir('reports/air_export_employee_kpis')
     filenames.remove('count_per.xlsx')
@@ -110,8 +99,3 @@ if st.session_state["authentication_status"]:
 
 
 
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
