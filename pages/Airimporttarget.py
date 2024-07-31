51bc58753c620248a898ebe8509a29c138e0c4b4
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-07-31 13:47:27 +0300

    Update Airimporttarget.py

--- pages/Airimporttarget.py
+++ pages/Airimporttarget.py
@@ -14,16 +14,7 @@ with open('config.yaml') as file:
 st.set_page_config(page_title='Genel Transport',page_icon="https://www.geneltransport.com.tr/wp-content/uploads/2021/03/favicon.png", layout='wide')
 st.image('https://www.geneltransport.com.tr/wp-content/uploads/2021/03/logo-color.png')
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['pre-authorized']
-)
 
-authenticator.login()
-if st.session_state["authentication_status"]:
 
     
 
@@ -108,8 +99,3 @@ if st.session_state["authentication_status"]:
 
 
 
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
