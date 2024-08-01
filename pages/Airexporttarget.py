b45050e10057ca5913908cb350d526c45ec0fed1
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-08-01 16:07:00 +0300

    Update Airexporttarget.py

--- pages/Airexporttarget.py
+++ pages/Airexporttarget.py
@@ -18,7 +18,16 @@ with open('config.yaml') as file:
 
 
 
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
 
+authenticator.login()
+if st.session_state["authentication_status"]:
 
     
 
@@ -36,7 +45,8 @@ with open('config.yaml') as file:
         st.page_link("pages/Clientanalitics.py",label="Client Offer/Success Analysis", icon="📈")
         st.page_link("pages/Clientaircustomer.py",label="Client Air Customer Offer Analysis", icon="📈")
 
-  
+    st.sidebar.write(f'Welcome *{st.session_state["name"]}*')
+    authenticator.logout("Logout", "sidebar")
 
     filenames = os.listdir('reports/air_export_employee_kpis')
     filenames.remove('count_per.xlsx')
@@ -100,6 +110,10 @@ with open('config.yaml') as file:
     
 
 
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
 
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
 
 
