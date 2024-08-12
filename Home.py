f6b235a838a5c7eb2946aafefa7558b905270f81
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-08-12 04:23:44 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,18 +10,18 @@ import numpy as np
 from streamlit_lightweight_charts import renderLightweightCharts
 from datetime import datetime
 import time
-with st.empty():
-    for seconds in range(5):
-        st.write(f"⏳ {seconds} seconds have passed")
-        time.sleep(1)
-    st.write("✔️ 5 sec over!")
+
 st.set_page_config(page_title='Genel Transport',page_icon="https://www.geneltransport.com.tr/wp-content/uploads/2021/03/favicon.png", layout='wide')
 st.image('https://www.geneltransport.com.tr/wp-content/uploads/2021/03/logo-color.png')
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 
-
+with st.empty():
+    for seconds in range(5):
+        st.write(f"⏳ {seconds} seconds have passed")
+        time.sleep(1)
+    st.write("✔️ 5 sec over!")
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
