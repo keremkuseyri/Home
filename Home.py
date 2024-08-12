5954fbb627840e69c2eb8cb19a92e5226ea02945
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-08-12 05:18:47 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -28,7 +28,7 @@ authenticator = stauth.Authenticate(
 
 
 authenticator.login()
-st.write(st.session_state["authentication_status"])
+st.experimental_rerun()
 if st.session_state["authentication_status"]:
 
 
