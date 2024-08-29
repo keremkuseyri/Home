142cce292ac7616ad1c3ffdf9dd4b6434a783fdd
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-08-29 12:57:04 +0300

    Update testdb2.py

--- pages/testdb2.py
+++ pages/testdb2.py
@@ -117,7 +117,7 @@ export_styled_df = style_dataframe(export_combined_df)
 
 # Display the styled DataFrames in Streamlit
 st.write("Import :")
-st.dataframe(import_styled_df, use_container_width=True, height= 800)
+st.dataframe(import_styled_df, use_container_width=True, height= 770)
 
 st.write("Export :")
-st.dataframe(export_styled_df, use_container_width=True,  height= 800)
+st.dataframe(export_styled_df, use_container_width=True,  height= 770)
