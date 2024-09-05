c496f78ffa9af3ad965424d1b2ae09c2ca507c3a
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-09-06 00:11:54 +0300

    Update Airexporttarget.py

--- pages/Airexporttarget.py
+++ pages/Airexporttarget.py
@@ -113,9 +113,17 @@ if st.session_state["authentication_status"]:
 
 
 elif st.session_state["authentication_status"] is False:
+
+
     st.error('Username/password is incorrect')
+    st.session_state.clear()  # Clears the entire session state
+    st.session_state["rerun_trigger"] = True
 
 elif st.session_state["authentication_status"] is None:
+
+
     st.warning('Please enter your username and password')
+    st.session_state.clear()  # Clears the entire session state
+    st.session_state["rerun_trigger"] = True
 
 
