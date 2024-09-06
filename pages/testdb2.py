9e4fe3b292d0f47515b3cf83b48ba15ebe527618
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-09-06 18:46:52 +0300

    Update testdb2.py

--- pages/testdb2.py
+++ pages/testdb2.py
@@ -232,7 +232,8 @@ if st.session_state["authentication_status"]:
     combined_html_table = create_html_table(import_combined_df, export_combined_df)
     
     # Display the combined HTML table in Streamlit
-    st.markdown(combined_html_table, unsafe_allow_html=True)
+    if st.session_state["pre-authorized"]:
+       st.markdown(combined_html_table, unsafe_allow_html=True)
 
 
 
