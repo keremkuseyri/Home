a1af14f9ce8baad22c0075de538c2b7ca5265d03
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-09-19 15:15:50 +0300

    Update Airexporttarget.py

--- pages/Airexporttarget.py
+++ pages/Airexporttarget.py
@@ -44,8 +44,8 @@ if st.session_state["authentication_status"]:
     with st.sidebar.expander("Air Customer Report ✈️"):
         st.page_link("pages/Clientanalitics.py",label="Client Offer/Success Analysis", icon="📈")
         st.page_link("pages/Clientaircustomer.py",label="Client Air Customer Offer Analysis", icon="📈")
-    with st.sidebar.expander("Air Import/Export Yearly 📊"):
-         st.page_link("pages/testdb2.py", label="Sea Import/Export Yearly Revenue 📊")
+    with st.sidebar.expander("Sea Import/Export Yearly 📊"):
+         st.page_link("pages/testdb2.py", label="Sea Import/Export Revenue List📊")
 
     st.sidebar.write(f'Welcome *{st.session_state["name"]}*')
     authenticator.logout("Logout", "sidebar")
