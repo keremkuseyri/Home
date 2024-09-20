6ecfb30497caea8bad944f9b906363b1f70b2338
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-09-20 21:45:44 +0300

    Update testdb2.py

--- pages/testdb2.py
+++ pages/testdb2.py
@@ -140,6 +140,7 @@ if st.session_state["authentication_status"]:
     
         # Create the DataFrame
         df = pd.DataFrame(combined_data, columns=columns, index=rows)
+        df = df.map(format_int_with_commas)
         return df
     
     # Create DataFrames for Import and Export data
