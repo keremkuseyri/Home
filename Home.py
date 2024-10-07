8580355e7e67201477641d86e02b62b07ac7c7ec
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-10-07 15:54:32 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,8 +16,8 @@ authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['pre-authorized']
+    config['cookie']['expiry_days']
+
 )
 
 authenticator.login()
