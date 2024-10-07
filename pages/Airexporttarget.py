bfd5e0c7787487c0960ad065e834465054320cb9
Kerem Kuseyri <122981034+keremkuseyri@users.noreply.github.com>
2024-10-07 15:56:33 +0300

    Update Airexporttarget.py

--- pages/Airexporttarget.py
+++ pages/Airexporttarget.py
@@ -22,8 +22,7 @@ authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['pre-authorized']
+    config['cookie']['expiry_days']
 )
 
 authenticator.login()
